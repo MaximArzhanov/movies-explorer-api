e8fdda6cb8ee283d32ed794c043b42a041909e1b
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-03 23:13:52 +0300

    feat: Установлен фреймворк Express.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000; // Нужно добавить в переменную окружения
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
