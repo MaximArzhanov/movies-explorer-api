e1465675c9969baab38f6dc71f7b5b0c7e989b92
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-04 00:34:16 +0300

    feat: Подключена база данных MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mongodb')
+  .then(() => console.log('Success: Database connected!'))
+  .catch((err) => console.log(`Error: ${err}`));
 
 const PORT = 3000; // Нужно добавить в переменную окружения
 
